297fc77c0125eebf76e55ff5383dd270d3314604
huashuimuju <414684208@qq.com>
2022-11-09 12:28:59 +0800

    hot-fxi  first-commit

--- src/main/java/com/feifei/App.java
+++ src/main/java/com/feifei/App.java
@@ -9,7 +9,8 @@ public class App
     public static void main( String[] args )
     {
 
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!2" );
+        System.out.println("Hello World!1" );
+        System.out.println("Hello World!2" );
+        System.out.println("hello world!3");
     }
 }
